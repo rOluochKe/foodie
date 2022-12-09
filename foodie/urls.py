2967aa5d7e9d866d0ff714cab9d7d0c28aca68c0
Raymond Oluoch <rasholuoch@gmail.com>
2022-12-09 11:56:08 +0300

    marketplace implementation

--- foodie/urls.py
+++ foodie/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
 
+    path('marketplace/', include('marketplace.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
