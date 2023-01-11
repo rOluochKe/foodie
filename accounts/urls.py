c5266df45f08f4ae98ebabdcacbd60d730c9b2b2
Raymond Oluoch <rasholuoch@gmail.com>
2023-01-11 11:52:53 +0300

    customers profile building

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
   path('reset_password/', views.reset_password, name='reset_password'),
 
   path('vendor/', include('vendor.urls')),
-  # path('customer/', include('customers.urls')),
+  path('customer/', include('customers.urls')),
   
 ]
